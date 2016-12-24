2a7e31a71bcd5020d5ded1b6cd38d981fba6df84
yuzuqiang <zuqiang77@gmail.com>
2016-12-24 17:03:13 +0800

    Update make_db_pickle.py

--- ProgrammingPython/Preview/make_db_pickle.py
+++ ProgrammingPython/Preview/make_db_pickle.py
@@ -1,5 +1,7 @@
-from initdata import db
+#from initdata import db
+import initdata
 import pickle
 dbfile = open('people-pickle', 'wb')
-pickle.dump(db, dbfile)
+#pickle.dump(db, dbfile)
+pickle.dump(initdata.db,dbfile)
 dbfile.close()
